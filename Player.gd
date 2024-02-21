950b267bce979ebfd88581f55b6bcb03d080ff3f
Jordan Flores <hwt12@txstate.edu>
2024-02-20 18:19:08 -0600

    Added sprinting logic

--- Player.gd
+++ Player.gd
@@ -1,12 +1,21 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
+const NORMAL_SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const MAX_STAMINA = 5
+const SPRINT_MULTIPLIER = 1.5
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var mouse_sensitivity = 0.2
+var speed = NORMAL_SPEED
+var stamina = MAX_STAMINA
+var is_sprinting = false
+var can_sprint = true
+
+
+
 
 func _ready():
 	# Capture the mouse
@@ -29,16 +38,36 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	# Sprinting logic
+	if Input.is_action_pressed("sprint") and stamina > 0 and can_sprint:
+		is_sprinting = true
+	else:
+		is_sprinting = false
+
+	if is_sprinting:
+		speed = NORMAL_SPEED * SPRINT_MULTIPLIER
+		stamina -= delta  # Stamina depletes continuously while sprinting
+		if stamina <= 0.0:
+			stamina = 0.0  # Clamp stamina to zero
+			is_sprinting = false
+			can_sprint = false
+
+	# Stamina regeneration logic
+	if not is_sprinting:
+		speed = NORMAL_SPEED
+		stamina += delta / 2
+		stamina = min(stamina, MAX_STAMINA)  # Clamp stamina to its maximum value
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("walk_left", "walk_right", "walk_foward", "walk_back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
