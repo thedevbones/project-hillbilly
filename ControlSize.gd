5ab2dea49ec8790498edaf8a50fc0e485ec539aa
lah136 <lah136@txstate.edu>
2024-02-22 13:09:20 -0600

    Implemented basic start and quit buttons on main menu.

--- ControlSize.gd
+++ ControlSize.gd
@@ -2,4 +2,8 @@ extends Control
 
 
 func _on_start_btn_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://world.tscn")
+
+
+func _on_quit_btn_pressed():
+	get_tree().quit()
