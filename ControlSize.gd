8568db5be61bc89be7abf6a36beb89f36922f120
lah136 <lah136@txstate.edu>
2024-02-22 12:55:55 -0600

    Began Start Menu Script

--- ControlSize.gd
+++ ControlSize.gd
@@ -1,11 +1,5 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _on_start_btn_pressed():
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
