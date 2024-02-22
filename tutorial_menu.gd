98e8ae91d4b31b3ce5b464aaa21f77770a82d61b
Diego L <d_l427@txstate.edu>
2024-02-22 16:31:13 -0600

    added functionality to "start" and "back to menu" buttons

--- tutorial_menu.gd
+++ tutorial_menu.gd
@@ -9,3 +9,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://world.tscn")
+
+
+func _on_back_to_menu_button_pressed():
+	get_tree().change_scene_to_file("res://main_menu.tscn")
