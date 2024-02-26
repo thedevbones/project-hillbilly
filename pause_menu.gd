c8251119795010ba12613ed8b0d017ff728ec28b
lah136 <lah136@txstate.edu>
2024-02-26 13:35:32 -0600

    Minor changes to Pause menu code

--- pause_menu.gd
+++ pause_menu.gd
@@ -3,11 +3,12 @@ extends ColorRect
 
 @onready var ResumeBtn: Button = find_child("ResumeBtn")
 @onready var QuitBtn: Button = find_child("QuitBtn")
+@onready var MenuBtn: Button = find_child("MenuBtn")
 
 func _ready() -> void:
 	ResumeBtn.pressed.connect(unpaused)
 	QuitBtn.pressed.connect(get_tree().quit)
-	
+	MenuBtn.pressed.connect(mainMenu)
 
 func unpaused():
 	get_tree().paused = false
@@ -19,5 +20,5 @@ func paused():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	show()
 
-func _on_menu_btn_pressed():
+func mainMenu():
 	get_tree().change_scene_to_file("res://main_menu.tscn")
