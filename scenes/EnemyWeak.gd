4d0704e177aa708aec13a6f3f776dc4f6a0066d2
Jordan Flores <hwt12@txstate.edu>
2024-03-25 00:52:43 -0500

    Added example weak enemy
    Mostly for testing changing values

---
+++ scenes/EnemyWeak.gd
@@ -0,0 +1,17 @@
+extends "res://scripts/EnemyBase.gd"
+
+func _ready():
+	spawn()
+	hit_audio = $BulletHit
+	death_audio = $Death
+	patrol_timer = $PatrolTimer
+
+
+func _on_death_finished():
+	queue_free()
+
+
+func _on_patrol_timer_timeout():
+	is_waiting = false
+	current_target = (current_target + 1) % patrol_points.size()
+	navigation_agent.set_target_position(patrol_points[current_target])
