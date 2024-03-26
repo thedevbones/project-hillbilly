b16feb7e70f5bfcfe82d82c4d4c781302d0311cb
lah136 <lah136@txstate.edu>
2024-03-25 19:53:35 -0500

    Made scripts to connect all the buttons

--- scripts/PauseMenu.gd
+++ scripts/PauseMenu.gd
@@ -4,7 +4,7 @@ extends ColorRect
 @onready var ResumeBtn: Button = find_child("ResumeBtn")
 @onready var QuitBtn: Button = find_child("QuitBtn")
 @onready var MenuBtn: Button = find_child("MenuBtn")
-@onready var InPaused = $PauseMain
+@onready var InMain = $PauseMain
 @onready var InSetting = $PauseSetting
 @onready var InAudio = $PauseAudio
 @onready var InVideo = $PausedVideo
@@ -13,6 +13,10 @@ func _ready() -> void:
 	ResumeBtn.pressed.connect(unpaused)
 	QuitBtn.pressed.connect(get_tree().quit)
 	MenuBtn.pressed.connect(mainMenu)
+	InMain.visible = true
+	InSetting.visible = false
+	InAudio.visible = false
+	InVideo.visible = false
 
 func unpaused():
 	get_tree().paused = false
@@ -23,8 +27,54 @@ func paused():
 	get_tree().paused = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	show()
+	
+func _on_settings_btn_pressed():
+	InMain.visible = false
+	InSetting.visible = true
+	InAudio.visible = false
+	InVideo.visible = false
 
 func mainMenu():
 	unpaused()
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	get_tree().change_scene_to_file("res://scenes/MenuMain.tscn")
+
+func _on_in_audio_setting_btn_pressed():
+	InMain.visible = false
+	InSetting.visible = false
+	InAudio.visible = true
+	InVideo.visible = false
+
+func _on_in_video_setting_btn_pressed():
+	InMain.visible = false
+	InSetting.visible = false
+	InAudio.visible = false
+	InVideo.visible = true
+
+
+func _on_back_pause_btn_pressed():
+	InMain.visible = true
+	InSetting.visible = false
+	InAudio.visible = false
+	InVideo.visible = false
+
+
+func _on_back_setting_btn_pressed():
+	InMain.visible = false
+	InSetting.visible = true
+	InAudio.visible = false
+	InVideo.visible = false
+
+
+func _on_back_setting_btn_2_pressed():
+	InMain.visible = false
+	InSetting.visible = true
+	InAudio.visible = false
+	InVideo.visible = false
+
+
+func _on_in_game_check_box_toggled(button_pressed):
+	if button_pressed:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
