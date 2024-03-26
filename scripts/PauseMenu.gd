16683e896a8f36e02f5d885d183dd2d9e3e2c616
lah136 <lah136@txstate.edu>
2024-03-26 12:44:14 -0500

    Connected audio sliders

--- scripts/PauseMenu.gd
+++ scripts/PauseMenu.gd
@@ -1,6 +1,5 @@
 extends ColorRect
 
-
 @onready var ResumeBtn: Button = find_child("ResumeBtn")
 @onready var QuitBtn: Button = find_child("QuitBtn")
 @onready var MenuBtn: Button = find_child("MenuBtn")
@@ -28,16 +27,16 @@ func paused():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	show()
 	
+func mainMenu():
+	unpaused()
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	get_tree().change_scene_to_file("res://scenes/MenuMain.tscn")
+	
 func _on_settings_btn_pressed():
 	InMain.visible = false
 	InSetting.visible = true
 	InAudio.visible = false
 	InVideo.visible = false
-
-func mainMenu():
-	unpaused()
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	get_tree().change_scene_to_file("res://scenes/MenuMain.tscn")
 
 func _on_in_audio_setting_btn_pressed():
 	InMain.visible = false
@@ -51,13 +50,11 @@ func _on_in_video_setting_btn_pressed():
 	InAudio.visible = false
 	InVideo.visible = true
 
-
 func _on_back_pause_btn_pressed():
 	InMain.visible = true
 	InSetting.visible = false
 	InAudio.visible = false
 	InVideo.visible = false
-
 
 func _on_back_setting_btn_pressed():
 	InMain.visible = false
@@ -65,16 +62,22 @@ func _on_back_setting_btn_pressed():
 	InAudio.visible = false
 	InVideo.visible = false
 
-
 func _on_back_setting_btn_2_pressed():
 	InMain.visible = false
 	InSetting.visible = true
 	InAudio.visible = false
 	InVideo.visible = false
 
-
 func _on_in_game_check_box_toggled(button_pressed):
 	if button_pressed:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+func _on_master_value_changed(value):
+	volume(0,value)
+
+func volume(bus_index, value):
+	AudioServer.set_bus_volume_db(bus_index, value)
+
+
