77e6a7cdfd06fc74be124b55fbaf341956f50b5d
lah136 <lah136@txstate.edu>
2024-03-29 10:06:46 -0500

    Made corrections to FX sound bus levels

--- scripts/PauseMenu.gd
+++ scripts/PauseMenu.gd
@@ -103,3 +103,7 @@ func _on_master_value_changed(value):
 func _on_music_value_changed(value):
 	AudioServer.set_bus_volume_db(MUSIC_BUS, linear_to_db(value)) 
 	AudioServer.set_bus_mute(MUSIC_BUS, value < .05)
+
+func _on_sound_fx_value_changed(value):
+	AudioServer.set_bus_volume_db(SFX_BUS, linear_to_db(value)) 
+	AudioServer.set_bus_mute(SFX_BUS, value < .05)
