93997273f0982f4482ba284c149806e4a0bd7c82
lah136 <lah136@txstate.edu>
2024-03-29 09:28:26 -0500

    updated FX slider on Main

--- scripts/MenuMain.gd
+++ scripts/MenuMain.gd
@@ -63,7 +63,8 @@ func _on_music_value_changed(value):
 	AudioServer.set_bus_mute(MUSIC_BUS, value < .05)
 
 func _on_sound_fx_value_changed(value):
-	volume(2,value)
+	AudioServer.set_bus_volume_db(SFX_BUS, linear_to_db(value)) 
+	AudioServer.set_bus_mute(SFX_BUS, value < .05)
 
 func _on_check_box_toggled(button_pressed):
 	if button_pressed:
