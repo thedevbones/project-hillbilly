9697ea57ef8fb6361522650864341d5855ee3528
Jordan Flores <hwt12@txstate.edu>
2024-03-28 21:06:51 -0500

    Refactored pistol script
    Minor change to ammo counter as well

--- scripts/Pistol.gd
+++ scripts/Pistol.gd
@@ -1,90 +1,24 @@
-extends Node3D
+extends "res://scripts/Weapon.gd"
 
-signal shot_fired
-signal reloaded
-
-@onready var camera = $".."
-@onready var player = $"../.."
-@onready var raycast = $"../HitScan"
-@onready var original_pos = position
-@onready var original_rot = rotation
-@onready var target_pos = original_pos
-@onready var target_rot = original_rot
-@onready var bob_max = position.y + 0.02
-@onready var bob_min = position.y - 0.02
-
-const MAX_AMMO = 8
-const RELOAD_TIME = 1.5
 const ADS_POS = Vector3(0, -0.4, -1.058)
 const ADS_ROT = Vector3(0, 0, 0)
 const BOB_SPEED = 0.05
 
-var ammo = MAX_AMMO
-var damage = 1
-var range = Vector3(1.0, 1.0, 100)
-var is_reloading = false
-var is_aiming = false
-var ranged = true
-var bob_up = true
-
-func shoot():
-	if ammo > 0 and not is_reloading:
-		player.can_switch = false
-		position.z += 0.2
-		$PistolFire.play()
-		muzzle_flash()
-		hitscan()
-		ammo -= 1
-		player.can_switch = true
-
-func reload():
-	var was_aiming = false
-	if not is_reloading and ammo < MAX_AMMO:
-		is_reloading = true
-		player.can_switch = false
-		
-		if is_aiming: 
-			was_aiming = true
-			aim()
-		
-		$PistolReload.play()
-		await get_tree().create_timer(RELOAD_TIME).timeout
-		
-		ammo = MAX_AMMO
-		is_reloading = false
-		player.can_switch = true
-		emit_signal("reloaded")
-		if was_aiming and not is_aiming: aim()
-
-func aim():
-	if not is_aiming: 
-		is_aiming = true
-		target_pos = ADS_POS
-		target_rot = ADS_ROT
-	else:
-		is_aiming = false
-		target_pos = original_pos
-		target_rot = original_rot
-
-func hitscan():
-	if not raycast.is_enabled():
-		return
-	
-	raycast.force_raycast_update()  # Updates the raycast immediately
-	if raycast.is_colliding():
-		var collider = raycast.get_collider()
-		if collider and collider.has_method("apply_damage"):
-			collider.apply_damage(damage)
-			# NOTE: followig code is unused atm but will be used for FX
-			# var collision_point = raycast.get_collision_point()
-			# var collision_normal = raycast.get_collision_normal()
-			# Call a function to create FX
-			# emit_signal("shot_fired", collision_point, collision_normal)
-
-func muzzle_flash():
-	$MuzzleLight.show()
-	await get_tree().create_timer(0.1).timeout
-	$MuzzleLight.hide()
+func _ready():
+	damage = 1
+	max_ammo = 8
+	ammo = max_ammo
+	reload_time = 1.5
+	range = Vector3(1.0, 1.0, 100)
+	ranged = true
+	original_pos = position
+	original_rot = rotation
+	target_pos = original_pos
+	target_rot = original_rot
+	ads_pos = ADS_POS
+	ads_rot = ADS_ROT
+	fire_sound = $PistolFire
+	reload_sound = $PistolReload
 
 func _process(delta):
 	if not visible: pass
@@ -120,6 +54,3 @@ func _process(delta):
 	else:
 		if position.y <= bob_min: bob_up = true
 		position.y -= bob_speed * delta
-
-func _on_visibility_changed():
-	if is_aiming and not visible: aim()
