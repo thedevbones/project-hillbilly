9538a130a6d4273bd1bc8ee83a1529717ed44a7c
Diego L <d_l427@txstate.edu>
2024-03-30 23:41:26 -0500

    Added Main Menu btn to both Control menus
    made the button functional

--- scenes/Tutorials/AttackControls.gd
+++ scenes/Tutorials/AttackControls.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
-
-
-
 func _on_main_controls_pressed():
 	get_tree().change_scene_to_file("res://scenes/Tutorials/MainControls.tscn")
+
+func _on_main_menu_pressed():
+	get_tree().change_scene_to_file("res://scenes/MenuMain.tscn")
--- scenes/Tutorials/MainControls.gd
+++ scenes/Tutorials/MainControls.gd
@@ -3,3 +3,7 @@ extends CanvasLayer
 
 func _on_attack_controls_pressed():
 	get_tree().change_scene_to_file("res://scenes/Tutorials/AttackControls.tscn")
+
+
+func _on_main_menu_pressed():
+	get_tree().change_scene_to_file("res://scenes/MenuMain.tscn")
