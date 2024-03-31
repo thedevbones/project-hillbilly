512bafba0d6f9e5eddeafe1f60557841358429c1
Jordan Flores <hwt12@txstate.edu>
2024-03-31 13:57:03 -0500

    Adjustments to previous enemy code
    Mainly commented out patrol/search functionality for wave mode. Can add back for story mode

--- scripts/EnemyBase.gd
+++ scripts/EnemyBase.gd
@@ -2,9 +2,9 @@ extends CharacterBody3D
 
 enum States { PATROL, COMBAT, SEARCH }
 
-@onready var player = %Player
+@onready var player = get_node("/root/World/Player")
 
-var state = States.PATROL
+var state = States.COMBAT
 var health = 5
 var speed = 1.0
 var combat_speed = 2.0
@@ -27,45 +27,46 @@ var death_audio: AudioStreamPlayer3D
 
 func _process(delta):
 	SimpleGrass.set_player_position(global_position)
-	match state:
-		States.PATROL:
-			patrol_behavior(delta)
-		States.COMBAT:
-			combat_behavior(delta)
-		States.SEARCH:
-			search_behavior(delta)
+	combat_behavior(delta)
+	#match state:
+		#States.PATROL:
+			#patrol_behavior(delta)
+		#States.COMBAT:
+			#combat_behavior(delta)
+		#States.SEARCH:
+			#search_behavior(delta)
 
 func spawn():
 	SimpleGrass.set_interactive(true)
 	navigation_agent = NavigationAgent3D.new()
 	add_child(navigation_agent)
-	generate_patrol_points()
-	if patrol_points.size() > 0:
-		navigation_agent.set_target_position(patrol_points[current_target])
-	if patrol_timer:
-		patrol_timer.wait_time = wait_time
+	#generate_patrol_points()
+	#if patrol_points.size() > 0:
+		#navigation_agent.set_target_position(patrol_points[current_target])
+	#if patrol_timer:
+		#patrol_timer.wait_time = wait_time
 
-func patrol_behavior(delta):
-	if patrol_points.size() <= 0 or is_waiting: return
-	
-	var player_position = player.global_transform.origin
-	var location = global_transform.origin
-	var target_location = patrol_points[current_target]
-	speed = 1
-	
-	if location.distance_to(target_location) <= arrived_distance:
-		is_waiting = true
-		patrol_timer.start()
-
-	var next_point = navigation_agent.get_next_path_position()
-	
-	if next_point != Vector3.INF:
-		var direction = (next_point - location).normalized()
-		velocity = direction * speed
-		move_and_slide()
-		if player_heard() or player_in_fov(direction):
-			state = States.COMBAT
-			return
+#func patrol_behavior(delta):
+	#if patrol_points.size() <= 0 or is_waiting: return
+	#
+	#var player_position = player.global_transform.origin
+	#var location = global_transform.origin
+	#var target_location = patrol_points[current_target]
+	#speed = 1
+	#
+	#if location.distance_to(target_location) <= arrived_distance:
+		#is_waiting = true
+		#patrol_timer.start()
+#
+	#var next_point = navigation_agent.get_next_path_position()
+	#
+	#if next_point != Vector3.INF:
+		#var direction = (next_point - location).normalized()
+		#velocity = direction * speed
+		#move_and_slide()
+		#if player_heard() or player_in_fov(direction):
+			#state = States.COMBAT
+			#return
 
 func combat_behavior(delta):
 	if player == null: return 
@@ -84,45 +85,45 @@ func combat_behavior(delta):
 			var direction = (next_point - location).normalized()
 			velocity = direction * speed
 			move_and_slide()
-			if not player_heard() and not player_in_fov(direction):
-				state = States.SEARCH
-				return
+			#if not player_heard() and not player_in_fov(direction):
+				#state = States.SEARCH
+				#return
 
 func search_behavior(delta):
 	state = States.PATROL
 	return
 
-func player_heard():
-	var detection_radius = 10.0
-	if not player.is_moving: return false
-	if state == States.COMBAT: detection_radius = detection_radius * 2.0
-	elif player.is_crouching: detection_radius = detection_radius / 2.0
-	elif player.is_sprinting: detection_radius = detection_radius * 2.0
-	if global_transform.origin.distance_to(player.global_transform.origin) < detection_radius:
-		return true
-	return false
-
-func player_in_fov(current_direction):
-	var direction_to_player = (player.global_transform.origin - global_transform.origin).normalized()
-	var distance_to_player = (player.global_transform.origin - global_transform.origin).length()
-	var angle_to_player = acos(current_direction.dot(direction_to_player))
-	angle_to_player = rad_to_deg(angle_to_player)
-	
-	var crouching_modifier = 1.0
-	if %Player.is_crouching: crouching_modifier = 0.5
-	if angle_to_player <= 30.0  * crouching_modifier:
-		if distance_to_player <= sight_distance * crouching_modifier:
-			if not player_obscured():
-				return true
-	return false
-
-func player_obscured():
-	var space_state = get_world_3d().direct_space_state
-	var ray_query = PhysicsRayQueryParameters3D.new()
-	ray_query.from = global_transform.origin
-	ray_query.to = %Player.global_transform.origin
-	var result = space_state.intersect_ray(ray_query)
-	return result and result.collider != %Player
+#func player_heard():
+	#var detection_radius = 10.0
+	#if not player.is_moving: return false
+	#if state == States.COMBAT: detection_radius = detection_radius * 2.0
+	#elif player.is_crouching: detection_radius = detection_radius / 2.0
+	#elif player.is_sprinting: detection_radius = detection_radius * 2.0
+	#if global_transform.origin.distance_to(player.global_transform.origin) < detection_radius:
+		#return true
+	#return false
+#
+#func player_in_fov(current_direction):
+	#var direction_to_player = (player.global_transform.origin - global_transform.origin).normalized()
+	#var distance_to_player = (player.global_transform.origin - global_transform.origin).length()
+	#var angle_to_player = acos(current_direction.dot(direction_to_player))
+	#angle_to_player = rad_to_deg(angle_to_player)
+	#
+	#var crouching_modifier = 1.0
+	#if %Player.is_crouching: crouching_modifier = 0.5
+	#if angle_to_player <= 30.0  * crouching_modifier:
+		#if distance_to_player <= sight_distance * crouching_modifier:
+			#if not player_obscured():
+				#return true
+	#return false
+#
+#func player_obscured():
+	#var space_state = get_world_3d().direct_space_state
+	#var ray_query = PhysicsRayQueryParameters3D.new()
+	#ray_query.from = global_transform.origin
+	#ray_query.to = %Player.global_transform.origin
+	#var result = space_state.intersect_ray(ray_query)
+	#return result and result.collider != %Player
 
 func attack_player():
 	pass
@@ -136,10 +137,10 @@ func die():
 	if death_audio: death_audio.play()
 	else: queue_free()
 
-func generate_patrol_points():
-	var rng = RandomNumberGenerator.new()
-	for i in range(number_of_patrol_points):
-		var random_direction = Vector3(rng.randf_range(-1, 1), 0, rng.randf_range(-1, 1)).normalized()
-		var random_distance = rng.randf_range(0, patrol_radius)
-		var patrol_point = global_transform.origin + random_direction * random_distance
-		patrol_points.append(patrol_point)
+#func generate_patrol_points():
+	#var rng = RandomNumberGenerator.new()
+	#for i in range(number_of_patrol_points):
+		#var random_direction = Vector3(rng.randf_range(-1, 1), 0, rng.randf_range(-1, 1)).normalized()
+		#var random_distance = rng.randf_range(0, patrol_radius)
+		#var patrol_point = global_transform.origin + random_direction * random_distance
+		#patrol_points.append(patrol_point)
