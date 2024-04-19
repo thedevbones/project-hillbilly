d9dd5b60c3ac999a4077b1f9c88a8d4485a22537
jd1568 <jd1568@txstate.edu>
2024-04-19 12:15:28 -0500

    Added functionality so that box moves when hit

---
+++ scripts/box.gd
@@ -0,0 +1,14 @@
+extends RigidBody3D
+
+
+func apply_damage(collision_point, collision_normal, force_magnitude):
+	move_box(collision_point, collision_normal, force_magnitude)
+
+func move_box(collision_point, collision_normal, force_magnitude):
+	var force_direction = -collision_normal.normalized()
+	print(force_direction)
+	apply_central_force(force_direction * force_magnitude)
+
+	# Option 2: Set the box's position or translation directly
+	# global_position = collision_point + collision_normal * 2 # Move the box 2 units away from the collision point
+
