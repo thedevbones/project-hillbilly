986c0ac5c5731509c8ef1fb0a10db4d715722a96
Gage Buckley <gagebuckley@gmail.com>
2024-04-21 22:29:56 -0500

    Created and added door asset

---
+++ scenes/Door.gd
@@ -0,0 +1,30 @@
+extends StaticBody3D
+
+@export var isLocked := false
+@export var needsKey := false
+@export var neededkey : StaticBody3D
+
+var isOpen = false;
+var canInteract = true;
+
+func _ready() -> void:
+	pass
+	
+func action_used():
+	if isLocked: 
+		#insert noise to show it is locked
+		if needsKey:
+			print("Needs a key")
+			if !is_instance_valid(neededkey):
+				print("Door Unlocked")
+				isLocked = false
+				open_contain()
+	elif !isOpen and canInteract : 
+		open_contain()
+
+func open_contain():
+	canInteract = false
+	var animPlayer = $/root/World/Door/door/doorAnimation
+	animPlayer.play("Open")
+	isOpen = true
+	
